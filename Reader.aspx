8ce49b57a167d71acdcfcc4b8da52b2390ea081a
shavoc <salmanhackathon@gmail.com>
2024-12-09 12:07:26 +0500

    Add files via upload

---
+++ Reader.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Reader.aspx.cs" Inherits="FileInclusion.Reader" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h2>Local File Inclusion Example</h2>
+            <label for="fileInput">Enter the file name:</label>
+            <input type="text" id="fileInput" name="fileInput" />
+            <button type="submit">Submit</button>
+            <br />
+            <h3>File Content:</h3>
+            <asp:Label ID="resultLabel" runat="server" Text=""></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Reader.aspx.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace FileInclusion
+{
+    public partial class Reader : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (IsPostBack)
+            {
+                string fileName = Request.Form["fileInput"];
+                ReadFile(fileName);
+                
+            }
+        }
+        public void ReadFile(string fileName)
+        {
+            try
+            {
+                string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, fileName);
+                string resolvedPath = Path.GetFullPath(filePath);
+                Response.AppendHeader("Content-Disposition", "attachment; filename=" + Path.GetFileName(resolvedPath));
+                Response.TransmitFile(resolvedPath);
+                Response.End();
+            }
+            catch (Exception ex)
+            {
+                
+            }
+        }
+
+        //public void ReadFilePatched(string fileName)
+        //{
+        //    try
+        //    {
+        //        string basePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Files");
+        //        string filePath = Path.Combine(basePath, fileName);
+        //        string resolvedPath = Path.GetFullPath(filePath);
+        //        if (!resolvedPath.StartsWith(basePath, StringComparison.OrdinalIgnoreCase))
+        //        {
+        //            Response.Write("Invalid file path.");
+        //            return;
+        //        }
+
+        //        if (File.Exists(resolvedPath))
+        //        {
+        //            Response.AppendHeader("Content-Disposition", "attachment; filename=" + Path.GetFileName(resolvedPath));
+        //            Response.TransmitFile(resolvedPath);
+        //            Response.End();
+        //        }
+        //        else
+        //        {
+        //            Response.Write("File does not exist.");
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        Response.Write("Error: " + ex.Message);
+        //    }
+        //}
+
+    }
+}
